d256522ea3c680736ec9ab6d120b828a3b7a1541
ASHA <p.ashapreethi@gmail.com>
2022-12-05 17:06:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 
 import React from "react";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { NavigationContainer} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./navigation/home";
-import Registration from "./navigation/registration";
-import Password from "./navigation/resetpwd";
+import Number from "./navigation/number";
+import Otp from "./navigation/otp";
 const Stack= createStackNavigator();
  
 function App(){
         return(
             <NavigationContainer>
-                <Stack.Navigator screenOptions={{header: ()=>null}}>
+                <Stack.Navigator screenOptions={{header : ()=> null}} >
                     <Stack.Screen name="home" component={Home}/>
-                    <Stack.Screen name="registration" component={Registration} />
-                    <Stack.Screen name="password" component={Password} />
+                    <Stack.Screen name="number" component={Number} />
+                    <Stack.Screen name="otp" component={Otp} />
                 </Stack.Navigator>
             </NavigationContainer>
         
@@ -22,3 +22,4 @@ function App(){
 
 export default App;
 
+//
--- navigation/home.js
+++ navigation/home.js
@@ -1,187 +1,92 @@
 import React from "react";
-import {View,Text,ScrollView,StyleSheet,TextInput, StatusBar,Pressable,Image} from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
-import Feather from "react-native-vector-icons/Feather";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import {View,Text,TextInput, StyleSheet,Image,Pressable, StatusBar} from "react-native";
+
 
 function Home({navigation}){
 
-    onRegister=()=>{
-         navigation.navigate("registration")
+    const onPresshandler=() => {
+        navigation.navigate("number"); 
     }
 
-    onReset=()=>{
-        navigation.navigate("password")
-    }
-    
-
-        return(
-        <ScrollView style={styles.bg}>
-            <StatusBar hidden={true} />
-            <Text style={styles.head}>Welcome Back!</Text>
-            <Text style={styles.para}>Please enter your account here</Text>
-            <View style={{marginTop:20}}>
-            <View style={styles.inputContainer}>
-                <Ionicons name="mail-outline"  size={20} style={{padding:8,}} color={"#000000"}/>
-                <TextInput placeholder="Email" style={styles.inputBox} />
+    return(
+        
+        <View style={styles.bg}>
+            <StatusBar backgroundColor="#ffffff" barStyle="dark-content"/>
+            <View style={{alignItems:"center",padding:10,margin:10}}>
+            <Image style={styles.img}
+        source={{
+          uri: 'https://contaque.com/contaquestatic/img/mobile-application.png',
+        }}
+      />
             </View>
-            <View style={styles.inputContainer}>
-                <SimpleLineIcons name="lock"  size={20} style={{padding:8,}} color={"#000000"}/>
-                <TextInput placeholder="Password" style={styles.inputBox} secureTextEntry />
-                <Feather name="eye"  size={20} style={{padding:10,}} color={"#000000"}/>
+            <Text style={styles.wishText}>Glad to see you:)</Text>
+            <View style={{flexDirection:"row",borderWidth:1,borderColor:"grey",height:50, marginLeft:17,marginRight:13,
+        padding:10}}>
+             <Text style={{backgroundColor:"#ffffff",fontSize:18}}>+91 </Text>   
+            <TextInput keyboardType="number-pad" style={styles.input} placeholder="| Mobile number" placeholderTextColor={"#d0d7e4"} />
             </View>
-            </View>
-            <Pressable onPress={onReset}>
-            <Text style={styles.forgotpwdTxt}>Forgot password?</Text>
+            <Pressable style={styles.btn} onPress={onPresshandler} >
+                <Text style={styles.btnTxt}>VERIFY</Text>
             </Pressable>
-            <Pressable style={styles.btn}>
-                <Text style={styles.btnTxt} >LOGIN</Text>
-            </Pressable>
-            <View style={{flexDirection: 'row', alignItems: 'center',marginTop:10,}}>
-             <View style={{flex: 1, height: 2, backgroundColor: '#dcdcdc',marginLeft:10,}} />
-                <View >
-            <Text style={{width: 150, textAlign: 'center',color:"#6c6c6c",fontWeight:"500",}}>Or continue with</Text>
-                </View>
-            <View style={{flex: 1, height: 2, backgroundColor: '#dcdcdc',marginRight:10,}} />
+            <View style={{flexDirection:"column",justifyContent:"flex-end",height:180,padding:10,}}>
+            <Text style={styles.termsText}>By signing up you agree to <Text style={styles.terms}>T&C</Text></Text>
             </View>
-            <View style={{marginTop:13}}>
-            <Pressable style={styles.mediaBtn}>
-                <FontAwesome5 name="facebook-f"  size={24}  color={"#39569c"} style={{padding:5,}}/>
-                <Text style={styles.fbTxt} >Facebook</Text>
-            </Pressable>
-            <Pressable style={styles.mediaBtn}>
-                <Image source={{uri:"https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"}} style={styles.img}/>
-                <Text style={styles.googleTxt} >Google</Text>
-            </Pressable>
-            </View>
-            <Text style={{color:"#626262",fontSize:13,textAlign:"center",fontWeight:"500",marginTop:10,}}>By signing up you accept the 
-            </Text>
-            <Text style={{ color:"#4657a4",fontSize:13,fontWeight:"700",textAlign:"center",marginTop:5,marginBottom:20,}}>Terms of Service 
-            <Text style={{color:"#626262"}}> and </Text> Privacy Policy</Text>
-            <View style={{flexDirection:"row",marginTop:20 , marginBottom:10,justifyContent:"center"}}>
-                <Text style={styles.paraBottom}>Don't have an account?</Text>
-                <Pressable onPress={onRegister}>
-                    <Text style={{ color:"#39569c",fontSize:14,fontWeight:"800",textAlign:"center",}} > Register</Text>
-                </Pressable>
-            </View>
-        </ScrollView>
-        )
+        </View>
+    )
 }
 
 export default Home;
 
 const styles=StyleSheet.create({
     bg:{
-        backgroundColor:"white",
+        backgroundColor:"#ffffff",
         flex:1,
-        padding:10,
-        borderRadius:10,
-        borderWidth:0,
-    },
-    head:{
-        color:"#212121",
-        fontSize:22,
-        marginTop:30,
-        padding:10,
-        textAlign:"center",
-        fontWeight:"500",
-    },
-    paraBottom:{
-        color:"#686868",
-        textAlign:"center",
-        fontSize:16,
-        fontWeight:"500",
-    },
-    para:{
-        color:"#acacac",
-        textAlign:"center",
-        fontSize:15,
-        fontWeight:"500",
-    },
-    inputContainer:{
-        flex:1,
-        flexDirection:"row",
-        backgroundColor:"#f8fafb",
-        justifyContent:"center",
-        borderRadius:7,
-        marginTop:10,
-        marginLeft:10,
-        marginRight:10,
-        height:50,
-        padding:5,
-    },
-    inputBox:{
-        flex:1,
-        backgroundColor:"#f8fafb",
-        borderRadius:10, 
-        fontSize:15,
-        color:"#000000",
-        fontWeight:"bold",
-
-    },
-    forgotpwdTxt:{
-        flex:1,
-        alignSelf:"flex-end",
-        color:"#4f5fa8",
-        fontSize:16,
-        marginTop:10,
-        marginRight:10,
-        fontFamily:"sans-serif-medium",
-        fontWeight:"700",
+      
     },
     btn:{
+        backgroundColor:"#3680ed",
+        textAlign:"center",
+        padding:10,
+        marginLeft:17,
+        marginRight:13,
+        justifyContent:"center",
         height:50,
-        flex:1,
-        alignSelf:"stretch",
-        borderRadius:7,
-        borderWidth:0,
-        marginTop:20,
-        marginLeft:10,
-        padding:15,
-        marginRight:10,
-        marginBottom:10,
-        backgroundColor:"#273b94",
+        marginTop:15,
+
     },
     btnTxt:{
-        color:"#dee1ee",
-        fontSize:15,
+        color:"#fefefe",
         textAlign:"center",
-     
+        fontSize:20,
     },
-    mediaBtn:{
-        height:60,
-        flex:1,
-        flexDirection:"row",
+    terms:{
+        textDecorationStyle:"solid",
+        textDecorationLine:"underline",
+        textDecorationColor:"#667281",
+    },
+    termsText:{
+        color:"#6d7886",
+        textAlign:"center",
+        fontSize:16,
+        justifyContent:"flex-end",
+    },
+    wishText:{
+        color:"#576375",
+        fontSize:26,
+        marginLeft:10,
+        padding:10,
+        marginTop:10,
+        fontWeight:"500",
+    },
+    img: {
         justifyContent:"center",
-        alignSelf:"stretch",
-        borderRadius:10,
-        borderWidth:0,
-        padding:10,
-        margin:10,
-        backgroundColor:"#f4f7fa",
-    },
-    fbTxt:{
-        color:"#5e6daf",
-        fontSize:16,
-        fontWeight:"600",
-        textAlign:"center",
-        padding:10,
-    },
-    googleTxt:{
-        color:"#4c4c4c",
-        fontSize:16,
-        textAlign:"center",
-        padding:10,
-        fontWeight:"600",
-    
-    },
-    img:{
-        height:40,
-        width:30,
-        padding:10,
-        marginRight:3,
-        
+        width: 300,
+        height:250,
+      },
+    input:{
+        padding:1,
+        fontSize:18,
+       
     }
-   
+
 })
